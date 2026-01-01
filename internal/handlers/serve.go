c05e1a1b2142769bd1364e42e1b2089ac29e6e9c
IlVin <vinokurov@corp.mail.ru>
2026-01-01 12:57:38 +0300

    Better arch

--- internal/handlers/serve.go
+++ internal/handlers/serve.go
@@ -8,6 +8,7 @@ import (
 
 // Объявляем список используемых параметров конфига
 type RouterConfig interface {
+	RouterType() string
 }
 
 type ServeConfig interface {
@@ -22,12 +23,24 @@ type MicroURLHandlers interface {
 	HndlDefault() http.HandlerFunc
 }
 
+// Фабрика роутеров
+// Возвращает роутер, заданный в конфиге
+func NewRouter(cfg RouterConfig, s MicroURLHandlers) http.Handler {
+	switch cfg.RouterType() {
+	case "mux":
+		return newMuxRouter(cfg, s)
+	case "chi":
+		return newChiRouter(cfg, s)
+	}
+	return newMuxRouter(cfg, s)
+}
+
 // Возвращает настроенный ServeMux
 // Для настройки нужен объект сервиса, который имеет известные методы.
 // А список известных методов описан в интерфейсе MicroURLHandlers
 // Зачем вообще нужно в роутер передавать объект с известными методами?
 // А затем, чтобы в тестах можно было подменить этот объект моком.
-func NewMuxRouter(cfg RouterConfig, s MicroURLHandlers) http.Handler {
+func newMuxRouter(cfg RouterConfig, s MicroURLHandlers) http.Handler {
 	mux := http.NewServeMux()
 	mux.HandleFunc("POST /{$}", s.HndlAddURL())
 	mux.HandleFunc("GET /{id}", s.HndlGetURL())
@@ -37,7 +50,7 @@ func NewMuxRouter(cfg RouterConfig, s MicroURLHandlers) http.Handler {
 }
 
 // Возвращает настроенный chi.Router
-func NewChiRouter(cfg RouterConfig, s MicroURLHandlers) http.Handler {
+func newChiRouter(cfg RouterConfig, s MicroURLHandlers) http.Handler {
 	r := chi.NewRouter()
 	r.Post("/", s.HndlAddURL())
 	r.Get("/{id}", s.HndlGetURL())
