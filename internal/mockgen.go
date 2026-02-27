2c2e942c4232c1c84e5540146620eaef442471d8
IlVin <vinokurov@corp.mail.ru>
2026-02-27 14:24:16 +0300

    mocks

--- internal/mockgen.go
+++ internal/mockgen.go
@@ -1,11 +1,14 @@
 package internal
 
-//go:generate $GOPATH/bin/mockgen                                                      -destination=mocks/pgx_mocks_test.go            -package=mocks github.com/jackc/pgx/v5 Tx,Row,BatchResults
-//go:generate $GOPATH/bin/mockgen -source=repository/pgc/backoff/backoff.go            -destination=mocks/pgc_backoff_mocks_test.go    -package=mocks
-//go:generate $GOPATH/bin/mockgen -source=repository/pgc/metrics/prometheus_metrics.go -destination=mocks/pgc_metrics_mocks_test.go    -package=mocks
-//go:generate $GOPATH/bin/mockgen -source=repository/pgc/instance/pg_instance.go       -destination=mocks/pgc_pginstance_mocks_test.go -package=mocks
-//go:generate $GOPATH/bin/mockgen -source=repository/pgc/pg_cluster.go                 -destination=mocks/pgc_pgcluster_mocks_test.go  -package=mocks
-//go:generate $GOPATH/bin/mockgen -source=repository/wal/wal.go                        -destination=mocks/wal_mocks_test.go            -package=mocks
-//go:generate $GOPATH/bin/mockgen -source=repository/repo_links.go                     -destination=mocks/repo_links_mocks_test.go     -package=mocks
-//go:generate $GOPATH/bin/mockgen -source=repository/inmem/core.go                     -destination=mocks/inmem_core_mocks_test.go     -package=mocks
-//go:generate $GOPATH/bin/mockgen -source=model/event/event.go                         -destination=mocks/event_mocks_test.go          -package=mocks
+//go:generate $GOPATH/bin/mockgen                                                      -destination=mocks/pgx_mock.go            -package=mocks github.com/jackc/pgx/v5 Tx,Row,BatchResults
+//go:generate $GOPATH/bin/mockgen -source=repository/pgc/backoff/backoff.go            -destination=mocks/pgc_backoff_mock.go    -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=repository/pgc/metrics/prometheus_metrics.go -destination=mocks/pgc_metrics_mock.go    -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=repository/pgc/instance/pg_instance.go       -destination=mocks/pgc_pginstance_mock.go -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=repository/pgc/pg_cluster.go                 -destination=mocks/pgc_pgcluster_mock.go  -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=repository/wal/wal.go                        -destination=mocks/wal_mock.go            -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=repository/repo_links.go                     -destination=mocks/repo_links_mock.go     -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=repository/inmem/core.go                     -destination=mocks/inmem_core_mock.go     -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=repository/repo.go                           -destination=mocks/repo_mock.go           -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=model/event/event.go                         -destination=mocks/event_mock.go          -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=service/service.go                           -destination=mocks/service_mock.go        -package=mocks
+//go:generate $GOPATH/bin/mockgen -source=handlers/handlers.go                         -destination=mocks/handlers_mock.go       -package=mocks
